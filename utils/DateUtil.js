bb813d157372c060f10b2fe3e3ca3fa10cf59537
gn5r <shangyuan.tuolang@gmail.com>
2020-08-23 11:01:42 +0900

    DateUtilを追加

--- utils/DateUtil.js
+++ utils/DateUtil.js
@@ -9,13 +9,27 @@ let DateUtil = {};
  * 
  * デフォルトは YYYY/MM/DD に変換する
  * @param {Date | String} source 変換元日付
- * @param {String} format フォーマット(詳しくはmomentのフォーマットを参照)
+ * @param {String} format  日付書式(詳しくはmomentのフォーマットを参照)
  * 
- * @return {String} 指定フォーマットに変換した日付
+ * @return {String} 指定した書式に変換した日付
  */
 DateUtil.format = (source = Date | String, format = "YYYY/MM/DD") => {
     if (!source) return "";
     return moment(new Date(source)).format(format);
+}
+
+/**
+ * 今日の日付を取得する
+ * 
+ * フォーマットを指定することで任意の書式で取得することも可能
+ * 
+ * デフォルトは YYYY/MM/DD
+ * @param {String} format 日付書式
+ * 
+ * @return {Date | String} 今日
+ */
+DateUtil.now = (format = "YYYY/MM/DD") => {
+    return moment(new Date()).format(format);
 }
 
 module.exports = DateUtil;
