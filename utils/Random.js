d5777e25539a5641c8263aab94f75c31b391872d
gn5r <shangyuan.tuolang@gmail.com>
2020-09-06 07:29:55 +0900

    乱数生成Utilを追加

---
+++ utils/Random.js
@@ -0,0 +1,17 @@
+let Random = {};
+
+/**
+ * 指定した範囲の乱数を生成する
+ *
+ * デフォルトは0～100
+ *
+ * @param {Number} start 開始
+ * @param {Number} end 終了
+ *
+ * @returns {Number} 指定した範囲の乱数
+ */
+Random.random = (start = 0, end = 100) => {
+  return Math.round(Math.random() * end) + start;
+};
+
+module.exports = Random;
